c6b5efa1930f04b99d34687596b63bf445e7cb38
Player3-1 <derinizlesin@gmail.com>
2026-01-27 18:24:03 +0300

    1.3

--- music.ts
+++
@@ -1,5 +0,0 @@
-// Music removed: stub left to prevent accidental imports
-export type TrackName = string;
-export function playTrack(_: any) { /* no-op */ }
-export function stop() { /* no-op */ }
-export default { playTrack, stop };
