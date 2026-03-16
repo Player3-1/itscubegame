07faa8a83658e044d4b5b40c92a02877fd87c89b
Player3-1 <derinizlesin@gmail.com>
2026-03-16 21:44:48 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { db } from './firebase.ts';
 import { collection, getDocs, doc, setDoc, updateDoc, getDoc, deleteDoc } from 'firebase/firestore';
 // expressions/emotes removed per user request
 
-// test yazısı 2
+// test yazısı ,
 
 // Start with empty levels
 const DEFAULT_LEVELS: LevelMetadata[] = [];
