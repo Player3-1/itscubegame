33284d7b3cb980dc0c3f05b3e1a541671a8c6bc7
Player3-1 <derinizlesin@gmail.com>
2026-03-16 22:33:29 +0300

    ol pls

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(process.cwd(), '.'),
         }
+      },
+      optimizeDeps: {
+        include: ['lucide-react']
       }
     };
 });
