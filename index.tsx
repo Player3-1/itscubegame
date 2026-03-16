44ba8fbbade9864eec5cc5d1dbce39926e512eef
Player3-1 <derinizlesin@gmail.com>
2026-03-16 21:27:25 +0300

    fix 1.4.1

--- index.tsx
+++ index.tsx
@@ -8,6 +8,8 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+// yenilensin diye yazıyom
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
